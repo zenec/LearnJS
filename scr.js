3ccacc1c6de81bd07c8a4a7d1179a83973db4428
Oleg Kostyuk <oi.kostyuk@mail.ru>
2018-11-30 03:42:32 +0300

    Изменил, с учетом того, что сообщения с вопросами должны задаваться пользователю два раза

--- scr.js
+++ scr.js
@@ -4,15 +4,17 @@ let money = prompt("Введите ваш бюджет на месяц", ""),
 let appData = {
     bank: money,
     timeDate: day,
-    expenses: {
-        expenditure: prompt("Введите обязательную статью расходов в этом месяце", ""),
-        cost: prompt("Во сколько обойдется?", "")
-    },
-    optionalExpenses: {
-
-    },
+    expenses: {},
+    optionalExpenses: {},
     income: [],
     savings: false
 };
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 //выводим на экран пользователя бюджет на один день
 alert( appData.bank / 30 );
