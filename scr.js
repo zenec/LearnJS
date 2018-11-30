e3fa51de2a4ce4bfd5e6cb76bd51c9ca0d604ff8
Oleg Kostyuk <oi.kostyuk@mail.ru>
2018-11-30 03:49:28 +0300

    Заменил содержимое на эталонное

--- scr.js
+++ scr.js
@@ -1,14 +1,15 @@
-'use strict'
-let money = prompt("Введите ваш бюджет на месяц", ""),
-    day = prompt("Введите дату в формате YYYY-MM-DD", "");
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
 let appData = {
-    bank: money,
-    timeDate: day,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
+
 let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
 	a2 = prompt("Во сколько обойдется?", ''),
 	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
@@ -16,5 +17,5 @@ let a1 = prompt("Введите обязательную статью расхо
 
 appData.expenses.a1 = a2;
 appData.expenses.a3 = a4;
-//выводим на экран пользователя бюджет на один день
-alert( appData.bank / 30 );
+
+alert(appData.budget / 30);
